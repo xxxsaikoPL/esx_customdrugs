0b2a324098662b16d9eea90a3f8a4970c6152419
xxxsaikoPL <47118547+xxxsaikoPL@users.noreply.github.com>
2019-01-29 01:37:11 +0100

    Add files via upload

---
+++ sprzedaz_czarna_kasa/server.lua
@@ -0,0 +1,184 @@
+ESX = nil
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local selling = false
+	local success = false
+	local copscalled = false
+	local notintrested = false
+  
+  RegisterNetEvent('drugs:trigger')
+  AddEventHandler('drugs:trigger', function()
+	selling = true
+	    if selling == true then
+			TriggerEvent('pass_or_fail')
+  			TriggerClientEvent("pNotify:SetQueueMax", source, "lmao", 1)
+  			TriggerClientEvent("pNotify:SendNotification", source, {
+            text = "Probójesz sprzedać narkotyki",
+            type = "error",
+            queue = "lmao",
+            timeout = 2500,
+            layout = "Centerleft"
+        	})
+ 	end
+end)
+
+RegisterServerEvent('fetchjob')
+AddEventHandler('fetchjob', function()
+	local xPlayer  = ESX.GetPlayerFromId(source)
+    TriggerClientEvent('getjob', source, xPlayer.job.name)
+end)
+
+
+  RegisterNetEvent('drugs:sell')
+  AddEventHandler('drugs:sell', function()
+  	local xPlayer = ESX.GetPlayerFromId(source)
+	local morf = xPlayer.getInventoryItem('morf_pooch').count
+	local koda = xPlayer.getInventoryItem('koda_pooch').count
+	local weed = xPlayer.getInventoryItem('weed_pooch').count
+	local amfa = xPlayer.getInventoryItem('amfa_pooch').count
+	local paymentm = math.random (400,900)
+	local paymento = math.random (300,800)
+	local paymentw = math.random (200,600)
+	local paymentc = math.random (800,1000)
+
+
+		if morf >= 1 and success == true then
+			 	TriggerClientEvent("pNotify:SetQueueMax", source, "lmao", 5)
+				TriggerClientEvent("pNotify:SendNotification", source, {
+					text = "Sprzedałeś paczkę morfiny za $" .. paymentc ,
+					type = "success",
+					progressBar = false,
+					queue = "lmao",
+					timeout = 2000,
+					layout = "CenterLeft"
+			})
+			TriggerClientEvent("animation", source)
+			xPlayer.removeInventoryItem('morf_pooch', 1)
+  			xPlayer.addMoney('black_money', paymentc)
+  			selling = false
+  		elseif weed >= 1 and success == true then
+  				TriggerClientEvent("pNotify:SetQueueMax", source, "lmao", 5)
+				TriggerClientEvent("pNotify:SendNotification", source, {
+					text = "Sprzedałeś sreberko z marycha za $" .. paymentw ,
+					type = "success",
+					progressBar = false,
+					queue = "lmao",
+					timeout = 2000,
+					layout = "CenterLeft"
+			})
+			TriggerClientEvent("animation", source)
+			TriggerClientEvent("test", source)
+  			xPlayer.removeInventoryItem('weed_pooch', 1)
+  			xPlayer.addMoney('black_money', paymentw)
+  			selling = false
+  		  elseif koda >= 1 and success == true then
+  				TriggerClientEvent("pNotify:SetQueueMax", source, "lmao", 5)
+				TriggerClientEvent("pNotify:SendNotification", source, {
+					text = "Sprzedałeś paczkę kodeiny za $" .. paymentm ,
+					type = "success",
+					progressBar = false,
+					queue = "lmao",
+					timeout = 2000,
+					layout = "CenterLeft"
+			})
+			TriggerClientEvent("animation", source)
+  			xPlayer.removeInventoryItem('koda_pooch', 1)
+  			xPlayer.addMoney('black_money', paymentm)
+  			selling = false
+  			elseif amfa >= 1 and success == true then
+  				TriggerClientEvent("pNotify:SetQueueMax", source, "lmao", 5)
+				TriggerClientEvent("pNotify:SendNotification", source, {
+					text = "Sprzedałeś paczke amfetaminy za $" .. paymento ,
+					type = "success",
+					progressBar = false,
+					queue = "lmao",
+					timeout = 2000,
+					layout = "CenterLeft"
+			})
+			TriggerClientEvent("animation", source)
+			xPlayer.removeInventoryItem('amfa_pooch', 1)
+  			xPlayer.addMoney('black_money', paymento)
+  			selling = false
+			elseif selling == true and success == false and notintrested == true then
+				TriggerClientEvent("pNotify:SetQueueMax", source, "lmao", 5)
+				TriggerClientEvent("pNotify:SendNotification", source, {
+					text = "Nie jestem zainteresowany",
+					type = "error",
+					progressBar = false,
+					queue = "lmao",
+					timeout = 2000,
+					layout = "CenterLeft"
+			})
+  			selling = false
+  			elseif koda < 1 and morf < 1 and weed < 1 and amfa < 1 then
+				TriggerClientEvent("pNotify:SetQueueMax", source, "lmao", 5)
+				TriggerClientEvent("pNotify:SendNotification", source, {
+				text = "Nie masz więcej narkotyków",
+				type = "error",
+				progressBar = false,
+				queue = "lmao",
+				timeout = 2000,
+				layout = "CenterLeft"
+			})
+			elseif copscalled == true and success == false then
+  				TriggerClientEvent("pNotify:SetQueueMax", source, "lmao", 5)
+				TriggerClientEvent("pNotify:SendNotification", source, {
+					text = "Dzwonię na policję!",
+					type = "error",
+					progressBar = false,
+					queue = "lmao",
+					timeout = 2000,
+					layout = "CenterLeft"
+			})
+			TriggerClientEvent("notifyc", source)
+  			selling = false
+  		end
+end)
+
+RegisterNetEvent('pass_or_fail')
+AddEventHandler('pass_or_fail', function()
+  		
+  		local percent = math.random(1, 11)
+
+  		if percent == 7 or percent == 8 or percent == 9 then
+  			success = false
+  			notintrested = true
+  		elseif percent ~= 8 and percent ~= 9 and percent ~= 10 and percent ~= 7 then
+  			success = true
+  			notintrested = false
+  		else
+  			notintrested = false
+  			success = false
+  			copscalled = true
+  		end
+end)
+
+RegisterNetEvent('sell_dis')
+AddEventHandler('sell_dis', function()
+		TriggerClientEvent("pNotify:SetQueueMax", source, "lmao", 5)
+		TriggerClientEvent("pNotify:SendNotification", source, {
+		text = "Odszedłeś za daleko",
+		type = "error",
+		progressBar = false,
+		queue = "lmao",
+		timeout = 2000,
+		layout = "CenterLeft"
+	})
+end)
+
+RegisterNetEvent('checkD')
+AddEventHandler('checkD', function()
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local koda = xPlayer.getInventoryItem('koda_pooch').count
+	local morf = xPlayer.getInventoryItem('morf_pooch').count
+	local weed = xPlayer.getInventoryItem('weed_pooch').count
+	local amfa = xPlayer.getInventoryItem('amfa_pooch').count
+
+	if koda >= 1 or morf >= 1 or weed >= 1 or amfa >= 1 then
+		TriggerClientEvent("checkR", source, true)
+	else
+		TriggerClientEvent("checkR", source, false)
+	end
+
+end)
